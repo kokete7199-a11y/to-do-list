c7ce5511ee1ac6ab6a91205de2ebed2f984e32e9
kokete7199-a11y <kokete7199@gmail.com>
2026-01-17 22:22:52 +0300

    to do list task

---
+++ todo.js
@@ -0,0 +1,42 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+const taskList = document.getElementById("taskList");
+const count = document.getElementById("count");
+
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const text = input.value.trim();
+
+  if (text === "") return;
+
+  tasks.push({ text });
+  input.value = "";
+  saveTasks();
+  showTasks();
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  saveTasks();
+  showTasks();
+}
+
+function showTasks() {
+  taskList.innerHTML = "";
+  count.textContent = tasks.length;
+
+  tasks.forEach((task, index) => {
+    taskList.innerHTML += `
+      <li>
+        ${task.text}
+        <button onclick="deleteTask(${index})">X</button>
+      </li>
+    `;
+  });
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+showTasks();
